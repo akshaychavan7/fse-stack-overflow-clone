1bd0e28ef9bb9a8d30e964290af5bd143cc5ed11
Akshay Chavan <chavan.ak@northeastern.edu>
2024-04-08 15:03:46 -0400

    update config file to generate SECRET KEY randomly

--- server/config.js
+++ server/config.js
@@ -1,11 +1,17 @@
-// Add configuration setting for your server to this file
+const generateSecretKey = () => {
+  const secretKey =
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+  return secretKey;
+};
+
 // uncomment for MONGO_URL for docker
 // const MONGO_URL = "mongodb://mongodb:27017/fake_so";
 // config for local testing, that is, without docker.
-const MONGO_URL = "mongodb://localhost:27017/fake_so_fse";
+const MONGO_URL = "mongodb://localhost:27017/fake_so";
 const CLIENT_URL = "http://localhost:3000";
 const port = 8000;
-const SECRET_KEY = "HAKUNA_MATATA";
+const SECRET_KEY = generateSecretKey();
 
 module.exports = {
   MONGO_URL,
