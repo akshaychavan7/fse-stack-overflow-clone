bb7f8db689f723a44d984b2ef517a716e8dcaf2e
Akshay Chavan <chavan.ak@northeastern.edu>
2024-04-14 13:09:48 -0400

    remove redundant import

--- server/server.js
+++ server/server.js
@@ -58,7 +58,6 @@ app.use("/login", loginController);
 app.use("/register", loginController);
 app.use("/moderator", modController);
 app.use("/user", userController);
-app.use("/user", userController);
 
 let server = app.listen(port, () => {
   console.log(`Server starts at http://localhost:${port}`);
