7fe225f15e7b1050a181941540f0e515a69e6616
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2024-04-23 17:58:24 -0400

    update config for docke

--- server/python utils/config.py
+++ server/python utils/config.py
@@ -1,5 +1,5 @@
-# MONGO_URI = "mongodb://mongodb:27017/"  # enable this for docker
-MONGO_URI = "mongodb://localhost:27017/"
+MONGO_URI = "mongodb://mongodb:27017/"  # enable this for docker
+# MONGO_URI = "mongodb://localhost:27017/"
 HOST = "0.0.0.0"
 PORT = 8001
 ORIGIN = "http://localhost:3000"
